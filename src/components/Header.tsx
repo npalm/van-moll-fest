035c4a6dfa06f32f383e5b25853205715c715a13
Niek Palm <niek.palm@philips.com>
2026-01-31 11:52:53 +0100

    style: change reset icon to eraser

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,11 +44,13 @@ export function Header({
                 strokeWidth={2}
                 viewBox="0 0 24 24"
               >
+                {/* Eraser icon */}
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                  d="M5.505 15.495l6.99-6.99a2 2 0 012.828 0l2.172 2.172a2 2 0 010 2.828l-6.99 6.99a2 2 0 01-2.828 0l-2.172-2.172a2 2 0 010-2.828z"
                 />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8 21h12" />
               </svg>
             </button>
           )}
